5375b007360686e2a5ceb56f21be64b64e02af16
Sakib fahmid <hasanfahmidul2002@gmail.com>
2025-11-10 19:42:25 +0600

    AUthenticate but only with Nav

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtAvFacqGY6mvd9fFCt1rtRo1VAJkhyac",
+  authDomain: "green-nest-6e0f2.firebaseapp.com",
+  projectId: "green-nest-6e0f2",
+  storageBucket: "green-nest-6e0f2.firebasestorage.app",
+  messagingSenderId: "858790493805",
+  appId: "1:858790493805:web:1ad85128b23a8859a54964"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
